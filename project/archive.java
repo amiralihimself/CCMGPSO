85c7affa8c666e0165bdf62d14d7bcba014babb8
Amirali <madaniamirali7@gmail.com>
2024-04-25 22:15:54 -0400

    implemented archive.java

---
+++ project/archive.java
@@ -0,0 +1,6 @@
+public class archive {
+    private int archiveSize;
+    private LinkedList<particle> archiveParticles = new LinkedList<>();
+    private int num_dimensions;
+    private int num_objectives;
+}
