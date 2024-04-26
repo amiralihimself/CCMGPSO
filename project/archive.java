66c1f44c1ac2ba43671c5328f998319afd6c1a0a
Amirali <madaniamirali7@gmail.com>
2024-04-26 00:12:31 -0400

    added new methods to archive.java

--- project/archive.java
+++ project/archive.java
@@ -1,6 +1,42 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 public class archive {
     private int archiveSize;
     private LinkedList<particle> archiveParticles = new LinkedList<>();
     private int num_dimensions;
     private int num_objectives;
+
+    public archive(int archiveSize, int num_dimensions, int num_objectives){
+        this.archiveSize=archiveSize;
+        new LinkedList<>();
+        this.num_dimensions=num_dimensions;
+        this.num_objectives=num_objectives;
+    }
+    public LinkedList<particle> getArchiveParticles() {
+        return archiveParticles;
+    }
+
+    public void removeTheWorstSolution(){
+        updateCrowdingDistances();
+        int indexToBeRemoved=0;
+        double minimumCrowdingDistance=0;
+        for (int particleIndex=0; particleIndex<archiveParticles.size();particleIndex++){
+            if(particleIndex==0){
+                indexToBeRemoved=0;
+                minimumCrowdingDistance=archiveParticles.get(0).crowdingDistance;
+            }
+            else{
+                if(archiveParticles.get(particleIndex).crowdingDistance<minimumCrowdingDistance){
+                    minimumCrowdingDistance=archiveParticles.get(particleIndex).crowdingDistance;
+                    indexToBeRemoved=particleIndex;
+
+                }
+            }
+        }
+
+        archiveParticles.remove(indexToBeRemoved);
+
+    }
+
 }
+
