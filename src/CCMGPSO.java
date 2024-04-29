6848ebea1b89b3045c4f1965a7e82522ada4e5ef
Amirali <madaniamirali7@gmail.com>
2024-04-29 11:46:52 -0400

    added a constructor method to CCMGPSO.java

--- src/CCMGPSO.java
+++ src/CCMGPSO.java
@@ -14,5 +14,43 @@ public class CCMGPSO extends cooperativeMGPSO{
     private int runNumber;
     private int[] temp;
     int counter;
+    public CCMGPSO(int numDimensions,
+                   int numObjectives,
+                   int numDimensionGroups,
+                   int numContextVectors,
+                   int iterationsPerObjective,
+                   problem optimizationProblem,
+                   int archiveSize,
+                   int tournamentSize,
+                   int numIterations,
+                   int numParticles, int runNumber){
+        this.numDimensionGroups=numDimensionGroups;
+        this.numDimensions=numDimensions;
+
+        this.c1=0.0;
+        this.c2=0.0;
+        this.c3=0.0;
+        this.omega=0.0;
+        this.numObjectives=numObjectives;
+        this.optimizationProblem=optimizationProblem;
+        this.tournamentSize=tournamentSize;
+        this.archiveSize=archiveSize;
+        Archive=new archive(archiveSize, numDimensions, numObjectives);
+        this.numIterations=numIterations;
+        typeOfFunction="CCMGPSO";
+        numOfEvaluations=0;
+        previousGlobalBest=new int[numObjectives];
+        this.miniSubSwarms=new swarm[numDimensionGroups];
+        swarmsNDimensional= new swarm[numObjectives];
+        this.numParticles=numParticles;
+        this.allContextVectors=new double[numContextVectors][numDimensions];
+        this.iterationsPerObjective= iterationsPerObjective ;
+        this.contextVectorObjectives=new double[numContextVectors][numObjectives];
+        this.runNumber=runNumber;
+        temp=new int[numContextVectors];
+        counter=0;
+
+    }
+
 
 }
