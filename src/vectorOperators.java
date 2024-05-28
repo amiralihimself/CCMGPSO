a21cbeeb920dddaabcc789538da3025fe47bb3e2
Amirali <madaniamirali7@gmail.com>
2024-05-28 10:29:40 -0400

    added a new method to vectorOperators.java

--- src/vectorOperators.java
+++ src/vectorOperators.java
@@ -9,7 +9,23 @@ import java.util.stream.IntStream;
 
 public class vectorOperators {
 
+    public double[] getMaximumOfEachObjective(particle[] particles, int numObjectives){
+        double[] arrayToReturn= new double[numObjectives];
 
+        for (int objectiveIndex=0; objectiveIndex< numObjectives; objectiveIndex++){
+            double maximumOfObjective=-1*Double.MAX_VALUE;
+            for(int particleIndex=0; particleIndex<particles.length; particleIndex++){
+                double[] objectiveVector=particles[particleIndex].getObjectives();
+                if(objectiveVector[objectiveIndex] > maximumOfObjective){
+                    maximumOfObjective=objectiveVector[objectiveIndex];
+                }
+
+            }
+            arrayToReturn[objectiveIndex]=maximumOfObjective;
+
+        }
+        return arrayToReturn;
+    }
 
 
 }
