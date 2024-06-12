c7ddc5eb17d968ccc6f0f7ad4f76446f0c649b11
Amirali <madaniamirali7@gmail.com>
2024-06-12 10:30:58 -0400

    updates problem.java

--- src/problem.java
+++ src/problem.java
@@ -1,4 +1,7 @@
-
+/* This is the abstract class for optimization problems.
+Specific optimization problems (such as the DTLZ suite, WFG test suite, etc are
+inheritors of this class
+ */
 public abstract class problem {
     protected String problemName;
 
